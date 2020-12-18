893a89fecb59f1ed108a81ab954562ec43313a7c
SpiderKisa <44308291+SpiderKisa@users.noreply.github.com>
2020-12-18 16:48:22 +1000

    rand

---
+++ RandGenerator.cpp
@@ -0,0 +1,19 @@
+﻿// RandGenerator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "num_threads.h"
+#include "interface.h"
+#include "experiment.h"
+#include <omp.h>
+#include <iostream>
+
+int main() {
+
+    unsigned T = get_num_threads();
+    size_t N = 10000000;
+    unsigned *V = new unsigned[N];
+    run_experiments_for(V, N, randomize, 10, 100);
+
+    return 0;
+}
